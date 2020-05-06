1078c7b3b49fb56f5a7c960c651cfea778c3cc6c
jordip <plana86@gmail.com>
2020-05-06 08:21:49 -0700

    Bulma.io added

--- admin/partials/common/nav.php
+++ admin/partials/common/nav.php
@@ -11,20 +11,42 @@
  */
 
 $tabs = array(
-	'pac'      => __( 'Settings', 'pac' ),
-	'pac-scan' => __( 'Scan and check', 'pac' ),
+	'pac'      => __('Settings', 'pac'),
+	'pac-scan' => __('Scan and check', 'pac'),
 );
 ?>
-<nav class="nav-tab-wrapper wp-clearfix">
-	<?php foreach ( $tabs as $url => $title ) : ?>
-		<a href="admin.php?page=<?php echo $url; ?>" class="nav-tab 
-										   <?php
-											if ( $url == $page ) :
-												?>
-			nav-tab-active
-												<?php
-								endif;
-											?>
-								"><?php echo $title; ?></a>
-	<?php endforeach; ?>
+
+<nav class="navbar" role="navigation" aria-label="main navigation">
+	<div class="navbar-brand">
+		<a class="navbar-item" href="#">
+			<img src="<?php echo plugins_url('../../assets/icon-128x128.png', dirname(__FILE__)); ?>" width="28" height="28">
+		</a>
+	</div>
+
+	<div id="pac-navBar" class="navbar-menu">
+		<div class="navbar-start">
+
+			<?php foreach ($tabs as $url => $title) : ?>
+				<a href="admin.php?page=<?php echo $url; ?>" class="navbar-item <?php if ($url == $page) : ?>is-active<?php endif; ?>"><?php echo $title; ?></a>
+			<?php endforeach; ?>
+		</div>
+		<div class="navbar-end">
+			<div class="navbar-item has-dropdown is-hoverable">
+				<a class="navbar-link">
+					More
+				</a>
+
+				<div class="navbar-dropdown is-right">
+					<a class="navbar-item" target="_blank" href="https://wordpress.org/plugins/product-availability-checker/">
+						About
+					</a>
+					<hr class="navbar-divider">
+					<a class="navbar-item" target="_blank" href="https://wordpress.org/support/plugin/product-availability-checker/">
+						Report an issue
+					</a>
+				</div>
+			</div>
+		</div>
+
+	</div>
 </nav>
--- includes/class-pac.php
+++ includes/class-pac.php
@@ -15,215 +15,228 @@
  * @author     Jordi Plana <hello@jordiplana.com>
  */
 
-class Pac {
+class Pac
+{
 
 
-	/**
-	 * The loader that's responsible for maintaining and registering all hooks that power
-	 * the plugin.
-	 *
-	 * @since  1.0.0
-	 * @access protected
-	 * @var    Pac_Loader    $loader    Maintains and registers all hooks for the plugin.
-	 */
-	protected $loader;
+    /**
+     * The loader that's responsible for maintaining and registering all hooks that power
+     * the plugin.
+     *
+     * @since  1.0.0
+     * @access protected
+     * @var    Pac_Loader    $loader    Maintains and registers all hooks for the plugin.
+     */
+    protected $loader;
 
-	/**
-	 * The unique identifier of this plugin.
-	 *
-	 * @since  1.0.0
-	 * @access protected
-	 * @var    string    $plugin_name    The string used to uniquely identify this plugin.
-	 */
-	protected $plugin_name;
+    /**
+     * The unique identifier of this plugin.
+     *
+     * @since  1.0.0
+     * @access protected
+     * @var    string    $plugin_name    The string used to uniquely identify this plugin.
+     */
+    protected $plugin_name;
 
-	/**
-	 * The current version of the plugin.
-	 *
-	 * @since  1.0.0
-	 * @access protected
-	 * @var    string    $version    The current version of the plugin.
-	 */
-	protected $version;
+    /**
+     * The current version of the plugin.
+     *
+     * @since  1.0.0
+     * @access protected
+     * @var    string    $version    The current version of the plugin.
+     */
+    protected $version;
 
-	/**
-	 * Define the core functionality of the plugin.
-	 *
-	 * Set the plugin name and the plugin version that can be used throughout the plugin.
-	 * Load the dependencies, define the locale, and set the hooks for the admin area and
-	 * the public-facing side of the site.
-	 *
-	 * @since 1.0.0
-	 */
-	public function __construct() {
-		if ( defined( 'PAC_VERSION' ) ) {
-			$this->version = PAC_VERSION;
-		} else {
-			$this->version = '1.0.0';
-		}
-		$this->plugin_name = 'pac';
+    /**
+     * Define the core functionality of the plugin.
+     *
+     * Set the plugin name and the plugin version that can be used throughout the plugin.
+     * Load the dependencies, define the locale, and set the hooks for the admin area and
+     * the public-facing side of the site.
+     *
+     * @since 1.0.0
+     */
+    public function __construct()
+    {
+        if (defined('PAC_VERSION')) {
+            $this->version = PAC_VERSION;
+        } else {
+            $this->version = '1.0.0';
+        }
+        $this->plugin_name = 'pac';
 
-		$this->load_dependencies();
-		$this->set_locale();
-		$this->define_admin_filters();
-		$this->define_admin_hooks();
-		$this->define_public_hooks();
-	}
+        $this->load_dependencies();
+        $this->set_locale();
+        $this->define_admin_filters();
+        $this->define_admin_hooks();
+        $this->define_public_hooks();
+    }
 
-	/**
-	 * Load the required dependencies for this plugin.
-	 *
-	 * Include the following files that make up the plugin:
-	 *
-	 * - Pac_Loader. Orchestrates the hooks of the plugin.
-	 * - Pac_i18n. Defines internationalization functionality.
-	 * - Pac_Admin. Defines all hooks for the admin area.
-	 * - Pac_Public. Defines all hooks for the public side of the site.
-	 *
-	 * Create an instance of the loader which will be used to register the hooks
-	 * with WordPress.
-	 *
-	 * @since  1.0.0
-	 * @access private
-	 */
-	private function load_dependencies() {
+    /**
+     * Load the required dependencies for this plugin.
+     *
+     * Include the following files that make up the plugin:
+     *
+     * - Pac_Loader. Orchestrates the hooks of the plugin.
+     * - Pac_i18n. Defines internationalization functionality.
+     * - Pac_Admin. Defines all hooks for the admin area.
+     * - Pac_Public. Defines all hooks for the public side of the site.
+     *
+     * Create an instance of the loader which will be used to register the hooks
+     * with WordPress.
+     *
+     * @since  1.0.0
+     * @access private
+     */
+    private function load_dependencies()
+    {
 
-		/**
-		 * The class responsible for orchestrating the actions and filters of the
-		 * core plugin.
-		 */
-		include_once plugin_dir_path( dirname( __FILE__ ) ) . 'includes/class-pac-loader.php';
+        /**
+         * The class responsible for orchestrating the actions and filters of the
+         * core plugin.
+         */
+        include_once plugin_dir_path(dirname(__FILE__)) . 'includes/class-pac-loader.php';
 
-		/**
-		 * The class responsible for defining internationalization functionality
-		 * of the plugin.
-		 */
-		include_once plugin_dir_path( dirname( __FILE__ ) ) . 'includes/class-pac-i18n.php';
+        /**
+         * The class responsible for defining internationalization functionality
+         * of the plugin.
+         */
+        include_once plugin_dir_path(dirname(__FILE__)) . 'includes/class-pac-i18n.php';
 
-		/**
-		 * The class responsible for defining all actions that occur in the admin area.
-		 */
-		include_once plugin_dir_path( dirname( __FILE__ ) ) . 'admin/class-pac-admin.php';
+        /**
+         * The class responsible for defining all actions that occur in the admin area.
+         */
+        include_once plugin_dir_path(dirname(__FILE__)) . 'admin/class-pac-admin.php';
 
-		/**
-		 * The class responsible for defining all actions that occur in the public-facing
-		 * side of the site.
-		 */
-		include_once plugin_dir_path( dirname( __FILE__ ) ) . 'public/class-pac-public.php';
+        /**
+         * The class responsible for defining all actions that occur in the public-facing
+         * side of the site.
+         */
+        include_once plugin_dir_path(dirname(__FILE__)) . 'public/class-pac-public.php';
 
-		/**
-		 * The class responsible for helper functions.
-		 */
-		include_once plugin_dir_path( dirname( __FILE__ ) ) . 'includes/class-pac-helper.php';
+        /**
+         * The class responsible for helper functions.
+         */
+        include_once plugin_dir_path(dirname(__FILE__)) . 'includes/class-pac-helper.php';
 
-		/**
-		 * The class responsible for Amazon PA API v5 functions.
-		 */
-		include_once plugin_dir_path( dirname( __FILE__ ) ) . 'includes/class-pac-paapi.php';
+        /**
+         * The class responsible for Amazon PA API v5 functions.
+         */
+        include_once plugin_dir_path(dirname(__FILE__)) . 'includes/class-pac-paapi.php';
 
-		$this->loader = new Pac_Loader();
-	}
+        $this->loader = new Pac_Loader();
+    }
 
-	/**
-	 * Define the locale for this plugin for internationalization.
-	 *
-	 * Uses the Pac_i18n class in order to set the domain and to register the hook
-	 * with WordPress.
-	 *
-	 * @since  1.0.0
-	 * @access private
-	 */
-	private function set_locale() {
+    /**
+     * Define the locale for this plugin for internationalization.
+     *
+     * Uses the Pac_i18n class in order to set the domain and to register the hook
+     * with WordPress.
+     *
+     * @since  1.0.0
+     * @access private
+     */
+    private function set_locale()
+    {
 
-		$plugin_i18n = new Pac_i18n();
+        $plugin_i18n = new Pac_i18n();
 
-		$this->loader->add_action( 'plugins_loaded', $plugin_i18n, 'load_plugin_textdomain' );
-	}
+        $this->loader->add_action('plugins_loaded', $plugin_i18n, 'load_plugin_textdomain');
+    }
 
-	/**
-	 * Register all of the filters related to the admin area functionality
-	 * of the plugin.
-	 *
-	 * @since  1.1.0
-	 * @access private
-	 */
-	private function define_admin_filters() {
-		$plugin_admin = new Pac_Admin( $this->get_plugin_name(), $this->get_version() );
+    /**
+     * Register all of the filters related to the admin area functionality
+     * of the plugin.
+     *
+     * @since  1.1.0
+     * @access private
+     */
+    private function define_admin_filters()
+    {
+        $plugin_admin = new Pac_Admin($this->get_plugin_name(), $this->get_version());
 
-		$this->loader->add_filter( 'after_setup_theme', $plugin_admin, 'admin_add_filters' );
-	}
+        $this->loader->add_filter('after_setup_theme', $plugin_admin, 'admin_add_filters');
+    }
 
-	/**
-	 * Register all of the hooks related to the admin area functionality
-	 * of the plugin.
-	 *
-	 * @since  1.0.0
-	 * @access private
-	 */
-	private function define_admin_hooks() {
+    /**
+     * Register all of the hooks related to the admin area functionality
+     * of the plugin.
+     *
+     * @since  1.0.0
+     * @access private
+     */
+    private function define_admin_hooks()
+    {
 
-		$plugin_admin = new Pac_Admin( $this->get_plugin_name(), $this->get_version() );
+        $plugin_admin = new Pac_Admin($this->get_plugin_name(), $this->get_version());
 
-		$this->loader->add_action( 'admin_enqueue_scripts', $plugin_admin, 'enqueue_styles' );
-		$this->loader->add_action( 'admin_enqueue_scripts', $plugin_admin, 'enqueue_scripts' );
-		$this->loader->add_action( 'admin_menu', $plugin_admin, 'add_options_page' );
-		$this->loader->add_action( 'admin_init', $plugin_admin, 'register_setting' );
-		$this->loader->add_action( 'wp_ajax_get_post_ids', $plugin_admin, 'ajax_get_post_ids' );
-		$this->loader->add_action( 'wp_ajax_get_post_info', $plugin_admin, 'ajax_get_post_info' );
-	}
+        $this->loader->add_action('admin_enqueue_scripts', $plugin_admin, 'register_styles');
+        $this->loader->add_action('admin_enqueue_scripts', $plugin_admin, 'register_scripts');
+        $this->loader->add_action('admin_enqueue_scripts', $plugin_admin, 'enqueue_styles');
+        $this->loader->add_action('admin_enqueue_scripts', $plugin_admin, 'enqueue_scripts');
+        $this->loader->add_action('admin_menu', $plugin_admin, 'add_options_page');
+        $this->loader->add_action('admin_init', $plugin_admin, 'register_setting');
+        $this->loader->add_action('wp_ajax_get_post_ids', $plugin_admin, 'ajax_get_post_ids');
+        $this->loader->add_action('wp_ajax_get_post_info', $plugin_admin, 'ajax_get_post_info');
+    }
 
-	/**
-	 * Register all of the hooks related to the public-facing functionality
-	 * of the plugin.
-	 *
-	 * @since  1.0.0
-	 * @access private
-	 */
-	private function define_public_hooks() {
+    /**
+     * Register all of the hooks related to the public-facing functionality
+     * of the plugin.
+     *
+     * @since  1.0.0
+     * @access private
+     */
+    private function define_public_hooks()
+    {
 
-		$plugin_public = new Pac_Public( $this->get_plugin_name(), $this->get_version() );
+        $plugin_public = new Pac_Public($this->get_plugin_name(), $this->get_version());
 
-		$this->loader->add_action( 'wp_enqueue_scripts', $plugin_public, 'enqueue_styles' );
-		$this->loader->add_action( 'wp_enqueue_scripts', $plugin_public, 'enqueue_scripts' );
-	}
+        $this->loader->add_action('wp_enqueue_scripts', $plugin_public, 'enqueue_styles');
+        $this->loader->add_action('wp_enqueue_scripts', $plugin_public, 'enqueue_scripts');
+    }
 
-	/**
-	 * Run the loader to execute all of the hooks with WordPress.
-	 *
-	 * @since 1.0.0
-	 */
-	public function run() {
-		$this->loader->run();
-	}
+    /**
+     * Run the loader to execute all of the hooks with WordPress.
+     *
+     * @since 1.0.0
+     */
+    public function run()
+    {
+        $this->loader->run();
+    }
 
-	/**
-	 * The name of the plugin used to uniquely identify it within the context of
-	 * WordPress and to define internationalization functionality.
-	 *
-	 * @since  1.0.0
-	 * @return string    The name of the plugin.
-	 */
-	public function get_plugin_name() {
-		return $this->plugin_name;
-	}
+    /**
+     * The name of the plugin used to uniquely identify it within the context of
+     * WordPress and to define internationalization functionality.
+     *
+     * @since  1.0.0
+     * @return string    The name of the plugin.
+     */
+    public function get_plugin_name()
+    {
+        return $this->plugin_name;
+    }
 
-	/**
-	 * The reference to the class that orchestrates the hooks with the plugin.
-	 *
-	 * @since  1.0.0
-	 * @return Pac_Loader    Orchestrates the hooks of the plugin.
-	 */
-	public function get_loader() {
-		return $this->loader;
-	}
+    /**
+     * The reference to the class that orchestrates the hooks with the plugin.
+     *
+     * @since  1.0.0
+     * @return Pac_Loader    Orchestrates the hooks of the plugin.
+     */
+    public function get_loader()
+    {
+        return $this->loader;
+    }
 
-	/**
-	 * Retrieve the version number of the plugin.
-	 *
-	 * @since  1.0.0
-	 * @return string    The version number of the plugin.
-	 */
-	public function get_version() {
-		return $this->version;
-	}
+    /**
+     * Retrieve the version number of the plugin.
+     *
+     * @since  1.0.0
+     * @return string    The version number of the plugin.
+     */
+    public function get_version()
+    {
+        return $this->version;
+    }
 }
